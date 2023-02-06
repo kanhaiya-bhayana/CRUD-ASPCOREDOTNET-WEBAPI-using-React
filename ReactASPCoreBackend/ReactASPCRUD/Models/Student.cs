954c2ec6bb34bf313de4abcde4eca95db4bcaec7
Kanhaiya <kbhayana60_be20@thapar.edu>
2023-02-07 00:46:41 +0530

    All crud opeartions are fixed

---
+++ ReactASPCoreBackend/ReactASPCRUD/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactASPCRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string studentName { get; set; }
+
+        public string course { get; set; }
+    }
+}
---
+++ ReactASPCoreBackend/ReactASPCRUD/Models/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactASPCRUD.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Student { get;set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=G1-1MS2MQ3-L;Initial Catalog=empentry; TrustServerCertificate=True; Integrated Security=True");
+
+        }
+    }
+}
